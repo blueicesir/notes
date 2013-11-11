dcef41f78edae2a7990445eba5459f1632998ef0
blueicesir <blueicesir@gmail.com>
2013-11-11 22:50:04 +0800

    Sqlite3 database write performance optimization

--- php/gdoa2sqlite3.php
+++ php/gdoa2sqlite3.php
@@ -13,6 +13,9 @@ $db->exec("create table gdoa(oa_id text,oa_name text,oa_com_name text,oa_dep_nam
 
 $dom=simplexml_load_file('/Users/blueice/git/gdoa.xml');
 
+$db->exec("PRAGMA synchronous = OFF;");
+$db->exec("begin;");
+
 foreach($dom->Table as $item)
 {
 	$sql="insert into gdoa values(";
@@ -23,9 +26,9 @@ foreach($dom->Table as $item)
 	$sql.="'".$item->OA_JOB_NAME."',";
 	$sql.="'".$item->OA_PHONE."',";
 	$sql.="'".$item->CU_MAIL."'";
-	$sql.=")";
+	$sql.=");";
 	$db->exec($sql);
 	echo $sql."\r\n";
 }
-
+$db->exec("commit;");
 ?>
